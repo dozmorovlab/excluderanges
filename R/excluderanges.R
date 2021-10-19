699dd28b9edb7ecb0a0aab21ea6db90bfd13eb53
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2021-10-18 21:30:13 -0400

    Add package documentation

---
+++ R/excluderanges.R
@@ -0,0 +1,12 @@
+#' excluderanges: Genomic coordinates of problematic genomic regions
+#'
+#' Genomic coordinates of problematic genomic regions that should be
+#' avoided when working with genomic data. GRanges of exclusion 
+#' regions (formerly known as blacklisted), centromeres, telomeres, known 
+#' heterochromatin regions, etc. (UCSC 'gap' table data). Primarily for human 
+#' and mouse genomes, hg19/hg38 and mm9/mm10 genome assemblies. 
+#'
+#' @docType package
+#' @name excluderanges
+NULL
+#> NULL
